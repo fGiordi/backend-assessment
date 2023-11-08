ebf02fafbd6900ccae6023d589efb36cf479ffc0
fGiordi <giordifungula@gmail.com>
2023-11-08 15:04:22 +0200

    added update and delete query

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,17 @@ import { startStandaloneServer } from '@apollo/server/standalone';
 const books = [
   {
     title: 'The Awakening',
-    author: 'Kate Chopin',
+    author: {name: 'Kate Chopin'},
   },
   {
     title: 'City of Glass',
-    author: 'Paul Auster',
+    author: {name: 'Paul Auster'},
   },
 ];
+
+const authors = [
+	{name: 'James Clear'}
+]
 
 // A schema is a collection of type definitions (hence "typeDefs")
 // that together define the "shape" of queries that are executed against
@@ -19,10 +23,15 @@ const books = [
 const typeDefs = `#graphql
   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
+	type Author {
+		name: String
+		books: [Book]
+	}
+
   # This "Book" type defines the queryable fields for every book in our data source.
   type Book {
     title: String
-    author: String
+    author: Author
   }
 
   # The "Query" type is special: it lists all of the available queries that
@@ -31,6 +40,18 @@ const typeDefs = `#graphql
   type Query {
     books: [Book]
   }
+	type Query {
+		books: [Book]
+		authors: [Author]
+	}
+	type Query {
+		authors: [Author]
+	}
+	type Mutation {
+		addBook(title: String, author: String): Book
+		updateBook(title: String, newTitle: String, author: String): Book
+    deleteBook(title: String, author: String): String
+	}
 `;
 
 // Resolvers define how to fetch the types defined in your schema.
@@ -38,8 +59,46 @@ const typeDefs = `#graphql
 const resolvers = {
   Query: {
     books: () => books,
+		authors: () => authors,
   },
-};
+	Mutation: {
+    addBook: (parent, args) => {
+      const { title, author } = args;
+      // Create a new book object and add it to the 'books' array
+      const newBook = {
+        title,
+        author: { name: author },
+      };
+      books.push(newBook);
+      return newBook;
+    },
+		updateBook: (parent, args) => {
+			const { title, newTitle, author } = args;
+			const bookIndex = books.findIndex((book) => book.title === title && book.author.name === author);
+			if (bookIndex === -1) {
+				throw new Error("Book not found");
+			}
+			const updatedBook = { ...books[bookIndex], title: newTitle };
+			books[bookIndex] = updatedBook;
+			return updatedBook;
+		},
+		deleteBook: (parent, args) => {
+			const { title, author } = args;
+			const bookIndex = books.findIndex((book) => book.title === title && book.author.name === author);
+			console.log('bookIndex')
+			if (bookIndex) {
+				const deletedBook = books[bookIndex];
+			books.splice(bookIndex, 1);
+			return books
+			} else {
+				throw new Error("Book not found");
+			}
+			
+		},
+  },
+	
+}
+
 
 // The ApolloServer constructor requires two parameters: your schema
 // definition and your set of resolvers.
