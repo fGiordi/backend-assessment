187e872ecaa299e572e2aeebfd95d5e2495473ab
fGiordi <giordifungula@gmail.com>
2023-11-12 23:12:36 +0200

    deployed cdk lambda function

---
+++ graph-api-cdk/test/graph-api-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GraphApiCdk from '../lib/graph-api-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/graph-api-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GraphApiCdk.GraphApiCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
---
+++ lambda/graphql.js
@@ -0,0 +1,9 @@
+"use strict";
+exports.handler = async function () {
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "text/plain" },
+        body: `Hello, world!`,
+    };
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiZ3JhcGhxbC5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbImdyYXBocWwudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLE9BQU8sQ0FBQyxPQUFPLEdBQUcsS0FBSztJQUNuQixPQUFPO1FBQ0wsVUFBVSxFQUFFLEdBQUc7UUFDZixPQUFPLEVBQUUsRUFBRSxjQUFjLEVBQUUsWUFBWSxFQUFFO1FBQ3pDLElBQUksRUFBRSxlQUFlO0tBQ3RCLENBQUM7QUFDSixDQUFDLENBQUMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnRzLmhhbmRsZXIgPSBhc3luYyBmdW5jdGlvbiAoKSB7XHJcbiAgICByZXR1cm4ge1xyXG4gICAgICBzdGF0dXNDb2RlOiAyMDAsXHJcbiAgICAgIGhlYWRlcnM6IHsgXCJDb250ZW50LVR5cGVcIjogXCJ0ZXh0L3BsYWluXCIgfSxcclxuICAgICAgYm9keTogYEhlbGxvLCB3b3JsZCFgLFxyXG4gICAgfTtcclxuICB9OyJdfQ==
